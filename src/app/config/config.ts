92d4d93308766fc5ac1838abfe3369c8425c5141
Ricardo <cr.calle@uniandes.edu.co>
2020-02-19 19:31:43 -0500

    Update config.ts

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,3 +1,3 @@
-export const URL_SERVICIOS = 'https://miso4101-201920-backend.herokuapp.com/';
+export const URL_SERVICIOS = 'http://miso4101-201920-backend.herokuapp.com/';
 // export const URL_SERVICIOS = 'http://project-backend-hmi.herokuapp.com';
 // export const URL_SERVICIOS = 'http://127.0.0.1:8000';
