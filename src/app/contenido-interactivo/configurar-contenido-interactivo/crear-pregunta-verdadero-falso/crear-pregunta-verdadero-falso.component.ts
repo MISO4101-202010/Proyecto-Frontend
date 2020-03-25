dceb1f8cae5f94aff3c8aa12ac7ac55da9135de2
Sebastian <js.millan10@uniandes.edu.co>
2020-03-25 12:27:14 -0500

    Pregunta verdadero o falso terminada La creación de preguntas de tipo verdadero o falso quedaron listas para las validaciones y pruebas

--- src/app/contenido-interactivo/configurar-contenido-interactivo/crear-pregunta-verdadero-falso/crear-pregunta-verdadero-falso.component.ts
+++ src/app/contenido-interactivo/configurar-contenido-interactivo/crear-pregunta-verdadero-falso/crear-pregunta-verdadero-falso.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
-import {FormBuilder, FormGroup, Validators, FormArray} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators, FormArray, FormControl} from '@angular/forms';
+import Swal from 'sweetalert2';
+import {ContenidoService} from 'src/app/services/contenido.service';
 
 export interface DialogData {
   marca: any;
@@ -14,16 +16,23 @@ export interface DialogData {
 export class CrearPreguntaVerdaderoFalsoComponent implements OnInit {
 
   questionForm: FormGroup;
+  respuestaControl = new FormControl('verdadero');
+  retroalimentacionControl = new FormControl('no');
+  saltarControl = new FormControl('si');
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData,
               private formBuilder: FormBuilder,
-              public dialogRef: MatDialogRef<CrearPreguntaVerdaderoFalsoComponent>) {
+              public dialogRef: MatDialogRef<CrearPreguntaVerdaderoFalsoComponent>,
+              private contenidoService: ContenidoService) {
     this.initializeForm();
   }
 
   initializeForm() {
     this.questionForm = this.formBuilder.group({
-      nombre: ['', [Validators.required]]
+      pregunta: ['', [Validators.required]],
+      nombre: ['', [Validators.required]],
+      retroalimentacion: [''],
+      numeroDeIntentos: ['', [Validators.required]]
     });
   }
 
@@ -34,7 +43,32 @@ export class CrearPreguntaVerdaderoFalsoComponent implements OnInit {
     this.dialogRef.close();
   }
 
-  crearMarca(){
+  crearMarca() {
+    if (this.questionForm.valid) {
+      const infoMarca = this.data.marca;
+      infoMarca.contenido = infoMarca.contenido_id;
+      const respuestaCorrecta = (this.respuestaControl.value === 'verdadero');
+      const tieneRetro = (this.retroalimentacionControl.value === 'si');
+      const saltar = (this.saltarControl.value === 'si');
+      this.questionForm.value.esVerdadero = respuestaCorrecta;
+      this.questionForm.value.puedeSaltar = saltar;
+      this.questionForm.value.tieneRetroalimentacion = tieneRetro;
+      this.questionForm.value.tipoActividad = 3;
+      this.contenidoService.agregarMarca(infoMarca).subscribe(result1 => {
+        this.questionForm.value.marca = result1.id;
+        this.contenidoService.agregarMarcaVerdaderoFalso(this.questionForm.value).subscribe(result2 => {
+          console.error(result2);
+          Swal.fire('Agregar Pregunta', 'Pregunta agregada correctamente', 'success');
+          this.dialogRef.close();
+        }, error => {
+          console.error(error);
+          Swal.fire('Oops...', 'Ocurrió un error agregando la marca, por favor inténtalo de nuevo', 'error');
+        });
+      }, error => {
+        console.error(error);
+        Swal.fire('Oops...', 'Ocurrió un error agregando la marca, por favor inténtalo de nuevo', 'error');
+      });
+    }
   }
 
 }
--- src/app/services/contenido.service.ts
+++ src/app/services/contenido.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { Observable, of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {environment} from 'src/environments/environment';
+import {Observable, of} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 
 @Injectable({
@@ -13,10 +13,13 @@ export class ContenidoService {
   private reportesUrl = `${environment.apiUrl}/activities/reports/`;
   private addPreguntaSelecconMultipleUrl = `${environment.apiUrl}/activities/generate-question-multiple-choice`;
   private addPreguntaAbiertaUrl = `${environment.apiUrl}/activities/generate-open-question`;
+  private addPreguntaFalsoVerdaderoUrl = `${environment.apiUrl}/activities/pregunta_f_v`;
   private detalleUrl = `${environment.apiUrl}/content/interactivecontent/`;
   private crearContenidoInteractivo = `${environment.apiUrl}/content/cont_interactivo`;
+  private crearMarca = `${environment.apiUrl}/activities/marca`;
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+  }
 
   getContenidos(): Observable<any> {
     return this.httpClient.get<any>(this.contenidoUrl);
@@ -44,15 +47,23 @@ export class ContenidoService {
     return this.httpClient.get<any>(this.detalleUrl + contentId);
   }
 
-  getReporteContenido(contentId: number): Observable<any>  {
+  getReporteContenido(contentId: number): Observable<any> {
     return this.httpClient.get<any>(this.reportesUrl + contentId);
   }
 
-  agregarMarcaPreguntaSeleccionMultiple(marca:any) : Observable<any> {
+  agregarMarcaPreguntaSeleccionMultiple(marca: any): Observable<any> {
     return this.httpClient.post(this.addPreguntaSelecconMultipleUrl, marca);
   }
 
-  agregarMarcaPreguntaAbierta(marca:any) : Observable<any> {
+  agregarMarcaPreguntaAbierta(marca: any): Observable<any> {
     return this.httpClient.post(this.addPreguntaAbiertaUrl, marca);
+  }
+
+  agregarMarcaVerdaderoFalso(pregunta: any): Observable<any> {
+    return this.httpClient.post(this.addPreguntaFalsoVerdaderoUrl, pregunta);
+  }
+
+  agregarMarca(marca: any): Observable<any> {
+    return this.httpClient.post(this.crearMarca, marca);
   }
 }
