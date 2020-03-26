1c2aa811ded54fa66478a3a9c76af988ef72b175
m.devia@uniandes.edu.co <m.devia@uniandes.edu.co>
2020-03-26 15:45:07 -0500

    new carousel

--- src/app/cursos/estudiante-cursos/estudiante-cursos.component.ts
+++ src/app/cursos/estudiante-cursos/estudiante-cursos.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {CursoService} from '../../services/curso.service';
-import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from '@angular/core';
+import { CursoService } from '../../services/curso.service';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-estudiante-cursos',
@@ -23,11 +23,40 @@ export class EstudianteCursosComponent implements OnInit {
     this.cursoService.getCursosEstudiante().subscribe(cursos => {
       console.log('data', cursos);
       this.cursos = cursos;
+      for (let i in this.cursos) {
+        this.cursos[i].currentIndex = 0;
+        this.cursos[i].mostrar = this.cursos[i].contenido_interactivo.slice(0, 3);
+      }
     });
   }
 
 
   getURLImageYoutube(url: string): string {
-    return 'http://img.youtube.com/vi/ID/0.jpg'.replace( 'ID', url.match(this.URL_REGEXP)[7]);
+    return 'http://img.youtube.com/vi/ID/0.jpg'.replace('ID', url.match(this.URL_REGEXP)[7]);
+  }
+
+  siguiente(curso) {
+
+    for (let i in this.cursos) {
+      if (this.cursos[i].nombre === curso.textContent) {
+        let tamaño = this.cursos[i].contenido_interactivo.length;
+        console.log(tamaño);
+        if (this.cursos[i].currentIndex < tamaño - 3) {
+          this.cursos[i].currentIndex += 1;
+          this.cursos[i].mostrar = this.cursos[i].contenido_interactivo.slice(this.cursos[i].currentIndex, this.cursos[i].currentIndex + 3);
+          this.cursos[i].currentIndex
+        }
+      }
+    }
+  }
+  anterior(curso) {
+    for (let i in this.cursos) {
+      if (this.cursos[i].nombre === curso.textContent) {
+        if (this.cursos[i].currentIndex) {
+          this.cursos[i].currentIndex -= 1;
+          this.cursos[i].mostrar = this.cursos[i].contenido_interactivo.slice(this.cursos[i].currentIndex, this.cursos[i].currentIndex + 3);
+        }
+      }
+    }
   }
 }
