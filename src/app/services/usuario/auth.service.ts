e5d244c90a2ea46216a0ddcb8a787369ead09842
mateodevia <m.devia@uniandes.edu.co>
2020-04-23 21:12:08 -0500

    log out service and method added to controller

--- src/app/services/usuario/auth.service.ts
+++ src/app/services/usuario/auth.service.ts
@@ -1,41 +1,40 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
-import { InfoLogin } from '../../models/infoLogin.model';
-import { AlumnoLogin } from '../../models/alumnoLogin.model';
-import { ProfesorLogin } from '../../models/profesorLogin.model';
-import { Login } from '../../models/login.model';
-import { Observable } from 'rxjs';
-import { retry, catchError, map } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
-
+import { Injectable } from "@angular/core";
+import { CanActivate, Router } from "@angular/router";
+import { HttpClient, HttpHeaders, HttpResponse } from "@angular/common/http";
+import { InfoLogin } from "../../models/infoLogin.model";
+import { AlumnoLogin } from "../../models/alumnoLogin.model";
+import { ProfesorLogin } from "../../models/profesorLogin.model";
+import { Login } from "../../models/login.model";
+import { Observable } from "rxjs";
+import { retry, catchError, map } from "rxjs/operators";
+import { environment } from "src/environments/environment";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthService implements CanActivate {
-
   authUrl = `${environment.apiUrl}/users/api-token-auth/`;
 
   // Http Headers
   httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type': 'application/json'
-    })
+      "Content-Type": "application/json",
+    }),
   };
   dataLog: InfoLogin = {
     userToken: null,
     isAlumno: null,
     dataAlumno: null,
-    dataProfesor: null
+    dataProfesor: null,
   };
 
   constructor(private router: Router, public http: HttpClient) {
-    console.log('se llamo el servicio');
+    console.log("se llamo el servicio");
   }
 
   login(usuario: Login): Observable<InfoLogin> {
-    return this.http.post(this.authUrl, JSON.stringify(usuario), this.httpOptions)
+    return this.http
+      .post(this.authUrl, JSON.stringify(usuario), this.httpOptions)
       .pipe(
         map((response: Response) => {
           this.dataLog.userToken = response.token;
@@ -50,12 +49,15 @@ export class AuthService implements CanActivate {
           return this.dataLog;
         }),
         retry(1),
-        catchError(err => {
-          console.log('Error en el login', err);
+        catchError((err) => {
+          console.log("Error en el login", err);
           return Observable.throw(err);
-        }
-        )
+        })
       );
+  }
+
+  public logout() {
+    sessionStorage.removeItem("userConectaTe");
   }
 
   getDatos(): InfoLogin {
@@ -63,14 +65,14 @@ export class AuthService implements CanActivate {
   }
 
   private storage(infoLogin: InfoLogin) {
-    sessionStorage.setItem('userConectaTe', JSON.stringify(infoLogin));
+    sessionStorage.setItem("userConectaTe", JSON.stringify(infoLogin));
   }
 
   getInfoLogin(): InfoLogin {
-    if (sessionStorage.getItem('userConectaTe')) {
-      this.dataLog = JSON.parse(sessionStorage.getItem('userConectaTe'));
+    if (sessionStorage.getItem("userConectaTe")) {
+      this.dataLog = JSON.parse(sessionStorage.getItem("userConectaTe"));
     } else {
-      console.log('key not exists');
+      console.log("key not exists");
     }
     return this.dataLog;
   }
@@ -82,7 +84,7 @@ export class AuthService implements CanActivate {
     const signedIn = !!this.dataLog.userToken;
     console.log("signedIn: ", signedIn);
     if (!signedIn) {
-      this.router.navigateByUrl('/login');
+      this.router.navigateByUrl("/login");
     }
     return signedIn;
   }
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
-import { FlatTreeControl } from '@angular/cdk/tree';
-import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
-import { Router } from '@angular/router';
-
+import { Component, OnInit } from "@angular/core";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import { Observable } from "rxjs";
+import { map, shareReplay } from "rxjs/operators";
+import { FlatTreeControl } from "@angular/cdk/tree";
+import {
+  MatTreeFlatDataSource,
+  MatTreeFlattener,
+} from "@angular/material/tree";
+import { Router } from "@angular/router";
+import { AuthService } from "src/app/services/usuario/auth.service";
 
 /**
  * Food data with nested structure.
@@ -18,12 +21,9 @@ interface NavNode {
 
 const TREE_DATA: NavNode[] = [
   {
-    name: 'Contenido Interactivo',
-    children: [
-      { name: 'Subir Video' },
-      { name: 'Ver Contenido Interactivo' },
-    ]
-  }
+    name: "Contenido Interactivo",
+    children: [{ name: "Subir Video" }, { name: "Ver Contenido Interactivo" }],
+  },
 ];
 
 /** Flat node with expandable and level information */
@@ -34,47 +34,57 @@ interface ExampleFlatNode {
 }
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"],
 })
 export class NavbarComponent {
-
   student: boolean;
+  user: any;
+  name: string;
+  showMenu: boolean;
 
   constructor(
     private breakpointObserver: BreakpointObserver,
-    public router: Router, ) {
+    public router: Router,
+    private auth: AuthService
+  ) {
     this.dataSource.data = TREE_DATA;
-    var actualUser = sessionStorage.getItem('userConectaTe');
+    var actualUser = sessionStorage.getItem("userConectaTe");
     this.student = JSON.parse(actualUser).isAlumno;
-
+    this.user = JSON.parse(actualUser);
+    this.name = "";
+    this.showMenu = false;
   }
 
   navNodeLinks(pag: string) {
     switch (pag) {
-      case 'Subir Video': {
-        this.router.navigate(['/load-videos']);
+      case "Subir Video": {
+        this.router.navigate(["/load-videos"]);
         break;
       }
-      case 'Ver Contenido Interactivo': {
-        this.router.navigate(['/contenido-interactivo']);
+      case "Ver Contenido Interactivo": {
+        this.router.navigate(["/contenido-interactivo"]);
         break;
       }
       default: {
-        console.log('Ruta no encontrada para la entrada: ', pag);
+        console.log("Ruta no encontrada para la entrada: ", pag);
         break;
       }
     }
   }
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  logout() {
+    console.log("LLEGO ACA");
+    this.auth.logout();
+  }
+
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
-
-
 
   private _transformer = (node: NavNode, level: number) => {
     return {
@@ -82,15 +92,20 @@ export class NavbarComponent {
       name: node.name,
       level: level,
     };
-  }
+  };
   treeControl = new FlatTreeControl<ExampleFlatNode>(
-    node => node.level, node => node.expandable);
+    (node) => node.level,
+    (node) => node.expandable
+  );
 
   treeFlattener = new MatTreeFlattener(
-    this._transformer, node => node.level, node => node.expandable, node => node.children);
+    this._transformer,
+    (node) => node.level,
+    (node) => node.expandable,
+    (node) => node.children
+  );
 
   dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
 
   hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
-
 }
