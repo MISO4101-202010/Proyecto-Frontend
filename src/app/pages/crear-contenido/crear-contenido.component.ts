8ba79b5dc9ffa7c1715418ec5539168c3369a2dd
mateodevia <m.devia@uniandes.edu.co>
2020-04-28 07:10:51 -0500

    single quotes

--- src/app/pages/crear-contenido/crear-contenido.component.ts
+++ src/app/pages/crear-contenido/crear-contenido.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit, Inject } from "@angular/core";
-import { LoadVideoService } from "../../services/contenidoInter/load-video.service";
+import { Component, OnInit, Inject } from '@angular/core';
+import { LoadVideoService } from '../../services/contenidoInter/load-video.service';
 import {
   MatDialog,
   MatDialogRef,
   MAT_DIALOG_DATA,
-} from "@angular/material/dialog";
-import { ContenidoService } from "../../services/contenido.service";
-import { ActivatedRoute, Router } from "@angular/router";
-import { LoadVideoComponent } from "../loadVideo/load-video.component";
-import Swal from "sweetalert2";
+} from '@angular/material/dialog';
+import { ContenidoService } from '../../services/contenido.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LoadVideoComponent } from '../loadVideo/load-video.component';
+import Swal from 'sweetalert2';
 
 @Component({
-  selector: "app-crear-contenido",
-  templateUrl: "./crear-contenido.component.html",
-  styleUrls: ["./crear-contenido.component.css"],
+  selector: 'app-crear-contenido',
+  templateUrl: './crear-contenido.component.html',
+  styleUrls: ['./crear-contenido.component.css'],
 })
 export class CrearContenidoComponent implements OnInit {
   constructor(
@@ -30,35 +30,35 @@ export class CrearContenidoComponent implements OnInit {
   loadContenido() {
     this._loadVideoService.getContenido().subscribe(
       (result) => {
-        console.log("ED: ", result);
+        console.log('ED: ', result);
         this.listContenido = result;
       },
       (error) => {
-        console.log("Edu: ", error);
+        console.log('Edu: ', error);
       },
       () => {}
     );
   }
 
   openModal(video): void {
-    console.log("llamado modal", video);
+    console.log('llamado modal', video);
     const dialogRef = this.dialog.open(ModalAsociarContenidoInt, {
-      width: "fit-content",
+      width: 'fit-content',
       data: { video: video.nombre, id: video.id },
     });
 
     dialogRef.afterClosed().subscribe((result) => {
-      console.log("The dialog was closed");
+      console.log('The dialog was closed');
     });
   }
 
   openAddVideoModal(): void {
     const dialogRef = this.dialog.open(LoadVideoComponent, {
-      width: "50%",
+      width: '50%',
     });
 
     dialogRef.afterClosed().subscribe((result) => {
-      console.log("The dialog was closed");
+      console.log('The dialog was closed');
       this.loadContenido();
     });
   }
@@ -66,8 +66,8 @@ export class CrearContenidoComponent implements OnInit {
 
 //modal
 @Component({
-  selector: "modal-AsoContInt",
-  templateUrl: "modal-asociar-contenido.html",
+  selector: 'modal-AsoContInt',
+  templateUrl: 'modal-asociar-contenido.html',
 })
 export class ModalAsociarContenidoInt {
   constructor(
@@ -82,9 +82,9 @@ export class ModalAsociarContenidoInt {
   }
 
   crearContenido(nombre, id, puedeSaltar) {
-    console.error("84", nombre.value, id);
-    if (!nombre.value || nombre.value === "") {
-      Swal.fire("Oops...", "Por favor ingresa un nombre", "error");
+    console.error('84', nombre.value, id);
+    if (!nombre.value || nombre.value === ') {
+      Swal.fire('Oops...', 'Por favor ingresa un nombre', 'error');
     } else {
       this._contenidoService
         .postContenidoInteractivo(nombre.value, id, puedeSaltar)
@@ -92,8 +92,8 @@ export class ModalAsociarContenidoInt {
           // tslint:disable-next-line: no-string-literal
           this.onNoClick();
           this.router.navigate([
-            "contenido-interactivo/configurar/",
-            result["id"],
+            'contenido-interactivo/configurar/',
+            result['id'],
           ]);
         });
     }
