1e697f75dfac754e3412705c2e7659662d2f0e35
SebastianMujica22 <42355577+SebastianMujica22@users.noreply.github.com>
2020-05-20 20:15:37 -0500

    Correccion

--- src/app/contenido-interactivo/configurar-contenido-interactivo/crear-seleccion-multiple/crear-seleccion-multiple.component.ts
+++ src/app/contenido-interactivo/configurar-contenido-interactivo/crear-seleccion-multiple/crear-seleccion-multiple.component.ts
@@ -1,10 +1,9 @@
-import { Component, Inject, OnInit } from "@angular/core";
-import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
-import { FormArray, FormBuilder, FormGroup, Validators, FormControl, FormGroupDirective, NgForm } from "@angular/forms";
-import Swal from "sweetalert2";
-import { ContenidoService } from "src/app/services/contenido.service";
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { FormArray, FormBuilder, FormGroup, Validators, FormControl, FormGroupDirective, NgForm } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { ContenidoService } from 'src/app/services/contenido.service';
 import { ErrorStateMatcher } from '@angular/material/core';
-
 
 export interface DialogData {
   marca: any;
@@ -12,18 +11,16 @@ export interface DialogData {
 }
 
 @Component({
-  selector: "app-crear-seleccion-multiple",
-  templateUrl: "./crear-seleccion-multiple.component.html",
-  styleUrls: ["./crear-seleccion-multiple.component.css"],
+  selector: 'app-crear-seleccion-multiple',
+  templateUrl: './crear-seleccion-multiple.component.html',
+  styleUrls: ['./crear-seleccion-multiple.component.css'],
 })
-
-
 
 export class CrearSeleccionMultipleComponent implements OnInit {
   questionForm: FormGroup;
   formArray = new FormArray([]);
-
-
+  minCaracteres = 5;
+  maxCaracteres = 200;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
@@ -38,7 +35,7 @@ export class CrearSeleccionMultipleComponent implements OnInit {
   }
 
   initializeForm() {
-    console.log("multiple", this.data);
+    console.log('multiple', this.data);
     if (this.data.marca.pregunta)
       this.data.marca.pregunta.opciones.forEach((e) => {
         let formGroup = new FormBuilder();
@@ -53,14 +50,14 @@ export class CrearSeleccionMultipleComponent implements OnInit {
 
     this.questionForm = this.formBuilder.group({
       marca_id: [
-        this.data.marca.pregunta ? this.data.marca.pregunta.marca : "",
+        this.data.marca.pregunta ? this.data.marca.pregunta.marca : '',
       ],
       seleccion_multiple_id: [
-        this.data.marca.pregunta ? this.data.marca.pregunta.id : "",
+        this.data.marca.pregunta ? this.data.marca.pregunta.id : '',
       ],
       enunciado: [
-        this.data.marca.pregunta ? this.data.marca.pregunta.enunciado : "",
-        [Validators.required, Validators.maxLength(200), Validators.minLength(5)],
+        this.data.marca.pregunta ? this.data.marca.pregunta.enunciado : '',
+        [Validators.required, Validators.maxLength(this.maxCaracteres), Validators.minLength(this.minCaracteres)],
       ],
       esMultipleResp: [
         this.data.marca.pregunta
@@ -69,8 +66,8 @@ export class CrearSeleccionMultipleComponent implements OnInit {
         [Validators.required],
       ],
       nombre: [
-        this.data.marca.pregunta ? this.data.marca.pregunta.nombre : "",
-        [Validators.required, Validators.maxLength(200), Validators.minLength(3)],
+        this.data.marca.pregunta ? this.data.marca.pregunta.nombre : '',
+        [Validators.required, Validators.maxLength(this.maxCaracteres), Validators.minLength(this.minCaracteres)],
       ],
       tieneRetroalimentacion: [
         this.data.marca.pregunta
@@ -89,11 +86,11 @@ export class CrearSeleccionMultipleComponent implements OnInit {
         : this.formBuilder.array([]),
     });
     if (this.data.tiene_retroalimentacion) {
-      this.questionForm.get("tieneRetroalimentacion").setValue(true);
+      this.questionForm.get('tieneRetroalimentacion').setValue(true);
     }
     if (!this.data.marca.pregunta) {
-      this.questionForm.removeControl("marca_id");
-      this.questionForm.removeControl("seleccion_multiple_id");
+      this.questionForm.removeControl('marca_id');
+      this.questionForm.removeControl('seleccion_multiple_id');
     }
   }
 
@@ -105,7 +102,7 @@ export class CrearSeleccionMultipleComponent implements OnInit {
     const opciones = this.questionForm.controls.opciones as FormArray;
     opciones.push(
       this.formBuilder.group({
-        opcion: ["", [Validators.required]],
+        opcion: ['', [Validators.required]],
         esCorrecta: false,
       })
     );
@@ -124,44 +121,44 @@ export class CrearSeleccionMultipleComponent implements OnInit {
   }
 
   agregarMarca() {
-    console.log("data", this.data);
-    console.log("form", this.questionForm.value);
+    console.log('data', this.data);
+    console.log('form', this.questionForm.value);
     if (this.questionForm.valid) {
       if (!this.validarUnaOpcion()) {
-        console.error("Al menos una opcion");
-        Swal.fire("Oops...", "Ingresa más de una opción", "error");
+        console.error('Al menos una opcion');
+        Swal.fire('Oops...', 'Ingresa más de una opción', 'error');
       } else if (!this.validarUnaCorrecta()) {
-        Swal.fire("Oops...", "Ingresa al menos una opción correcta", "error");
+        Swal.fire('Oops...', 'Ingresa al menos una opción correcta', 'error');
       } else {
         let texto;
         let texto2;
         let texto3;
         if (this.data.marca.pregunta) {
           delete this.data.marca.pregunta;
-          texto = "Actualizar Marca";
-          texto2 = "Marca actualizada correctamente";
-          texto3 = "actualizando";
+          texto = 'Actualizar Marca';
+          texto2 = 'Marca actualizada correctamente';
+          texto3 = 'actualizando';
         } else {
-          texto = "Agregar Marca";
-          texto2 = "Marca agregada correctamente";
-          texto3 = "agregando";
+          texto = 'Agregar Marca';
+          texto2 = 'Marca agregada correctamente';
+          texto3 = 'agregando';
         }
         this.questionForm.value.marca = this.data.marca;
         this.contenidoService
           .agregarMarcaPreguntaSeleccionMultiple(this.questionForm.value)
           .subscribe(
             (result) => {
-              Swal.fire(texto, texto2, "success");
+              Swal.fire(texto, texto2, 'success');
               this.dialogRef.close();
             },
             (error) => {
               console.error(error);
               Swal.fire(
-                "Oops...",
-                "Ocurrió un error " +
+                'Oops...',
+                'Ocurrió un error ' +
                   texto3 +
-                  " la marca, por favor inténtalo de nuevo",
-                "error"
+                  ' la marca, por favor inténtalo de nuevo',
+                'error'
               );
             }
           );
@@ -170,11 +167,7 @@ export class CrearSeleccionMultipleComponent implements OnInit {
   }
 
   quitarOpcion(i) {
-    const opciones = this.questionForm.get("opciones") as FormArray;
+    const opciones = this.questionForm.get('opciones') as FormArray;
     opciones.removeAt(i);
   }
 }
-
-
-
-
