f1ac48bc5eb54d816153757c89ab5a146cac4df5
Juan Sebastian Alvarez Eraso <juan_alvarez_12345@hotmail.com>
2020-05-24 00:03:47 -0500

    Agregada calificación por pregunta y calificación general al reporte del estudiante

--- src/app/contenido-interactivo/revision-content/revision-content.component.ts
+++ src/app/contenido-interactivo/revision-content/revision-content.component.ts
@@ -3,6 +3,7 @@ import { ContenidoService } from 'src/app/services/contenido.service';
 import { ActivatedRoute } from '@angular/router';
 import { ActivitiesService } from "src/app/services/activities-service/activities.service";
 import { OpcionesPreguntaMultiple } from "src/app/models/mark/optionsQuestionMultiple.model";
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-revision-content',
@@ -13,11 +14,12 @@ export class RevisionContentComponent implements OnInit {
 
   data;
   dataQuestions: Array<{
-    name: string; 
-    question: string; 
+    name: string;
+    question: string;
     arrayCorrectAnswers: Array<OpcionesPreguntaMultiple>
   }> = new Array();
   id: number;
+  total = 0;
 
   constructor(private contenidoService: ContenidoService,
      private activeRoute: ActivatedRoute,
@@ -40,13 +42,17 @@ export class RevisionContentComponent implements OnInit {
         });
       }
     });
+    // TODO pendiente de refactor
+    setTimeout(() => {
+      this.getTotal();
+    }, 1000);
   }
 
   getQuestion(idMarca) {
     let results = []
     this.activityService.getActivityById(idMarca).subscribe(
       data => {
-        data.forEach(o => {           
+        data.forEach(o => {
           results = results.concat(o.body.results)
         });
         if(results!== undefined && results[0]!== undefined){
@@ -57,21 +63,24 @@ export class RevisionContentComponent implements OnInit {
               result.opciones.forEach(option => {
                 if(option.esCorrecta === true){
                   arrayCorrect.push(
-                    {idOption: option.id, idQuestion: result.id, answerOption: false, titleOption: option.opcion});
+                    {idOption: option.id, idQuestion: result.id, answerOption: false, titleOption: option.opcion,
+                      qualification: result.qualification, type: result.type});
                   }
                 }
               );
-            }else if(result.type === 'preguntaAbierta'){
+            }else if(this.isOpenQuestion(result.type)){
               arrayCorrect.push(
-                {idOption: 0, idQuestion: result.id, answerOption: false, titleOption: 'No aplica'});
+                {idOption: 0, idQuestion: result.id, answerOption: false, titleOption: 'No aplica',
+                  qualification: this.getQualification(result), alreadyQualified: result.qualification !== undefined,
+                  type: result.type});
             }
-            
+
             this.dataQuestions.push(
             {
-                name: result.nombre, 
+                name: result.nombre,
                 question: result.enunciado,
                 arrayCorrectAnswers: arrayCorrect
-            });             
+            });
           }
         }
       },
@@ -85,8 +94,10 @@ export class RevisionContentComponent implements OnInit {
       let arrayCorrect = []
       console.log(data.body);
 
-      arrayCorrect.push({idOption: 0, idQuestion: data.body.id, answerOption: false, titleOption: data.body.esVerdadero?'Verdadero':'Falso'});
-        
+      arrayCorrect.push({idOption: 0, idQuestion: data.body.id, answerOption: false,
+        titleOption: data.body.esVerdadero ? 'Verdadero' : 'Falso',
+        qualification: data.body.qualification, type: data.body.type});
+
       this.dataQuestions.push(
           {
             name: data.body.nombre,
@@ -97,5 +108,27 @@ export class RevisionContentComponent implements OnInit {
         console.log('Error getting question information -> ', error);
       }
     );
-  }         
+  }
+
+  isOpenQuestion(type): boolean {
+    return type === 'preguntaAbierta';
+  }
+
+  getQualification(result): number {
+    return result.qualification === undefined ? 0 : result.qualification
+  }
+
+  getTotal() {
+    if (this.dataQuestions) {
+      const allQualifications = _.reduce(this.dataQuestions, function(acumulado, dq2) {
+        return acumulado
+          +
+          _.reduce(dq2.arrayCorrectAnswers, function (acumulado, ca2) {
+            return acumulado + ca2.qualification;
+          }, 0)
+      }, 0);
+
+      this.total = allQualifications / this.dataQuestions.length;
+    }
+  }
 }
